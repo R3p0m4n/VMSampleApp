e9ed192f101b1eb15ce04f7cdee60e2a00ca4d38
R3p0m4n <jos.hooker@gmail.com>
2011-12-29 19:49:34 -0500

    VMSampleApp initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VMSampleApp::Application.config.secret_token = '4d911a8a82275b54aef8fa5b65b308a1329bb5f11a0406e93489ddab1592d6faf33969b695de3dc7022ae36f34ea91fa00cb9bad9984b547cc6dddcbf859ff0d'
